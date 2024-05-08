561c9cdf760ef712306f8877546743d0573fe3be
Francisco <fj.trujillo.armenteros@gmail.com>
2024-05-08 18:14:30 +0200

    user1 añade package francisco y class Francisco

---
+++ Nombres_Participantes/src/francisco/Francisco.java
@@ -0,0 +1,15 @@
+package francisco;
+
+public class Francisco {
+	
+	public static void main(String[] args) {
+		Francisco francisco = new Francisco();
+	
+		francisco.teSaluda();
+		
+	}
+	 public void teSaluda() {
+		 System.out.println("Hola, soy Francisco");
+	 }
+	
+}
