3c9a4d4dfd7b5b15d65f353d5e2365e979313492
Pablo Verdugo <verdugo_pablo@hotmail.com>
2024-05-09 00:09:59 +0200

    usuario2 ha añadido su saludo

---
+++ Nombres_Participantes/src/Pablo/pablo.java
@@ -0,0 +1,15 @@
+package pablo;
+
+public class Pablo {
+
+    public static void main(String[] args) {
+        Pablo pablo = new Pablo();
+
+        pablo.teSaluda();
+
+    }
+
+    public void teSaluda() {
+        System.out.println("Hola Tomas , soy Pablo");
+    }
+}
